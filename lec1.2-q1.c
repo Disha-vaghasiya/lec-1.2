d8679c72252985e773ff8ea193dbb53ba41894fc
dishaa <sondagaryashvi@gmail.com>
2023-12-25 13:53:25 +0530

    lecture-1.2

---
+++ lec1.2-q1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+void main()
+{
+  
+   printf("Myself Disha vaghasiya\n");
+   printf("I'm 19 years old\n");
+   printf("I have studied from v.d.galiyara vidhyalaya\n");
+   printf("recently I am studying full stack development\n");
+   printf("Grid:5664\n");
+   printf("I m from surat");
+
+
+
+}
---
+++ lec1.2-q2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+void main(){
+
+
+  printf("- - - - - - - -\n");
+  printf("|\t      |\n");
+  printf("R\t      |\n");
+  printf("N\t      |\n");
+  printf("W\t      |\n");
+  printf("|\t      |\n");
+  printf("- - - - - - - -\n");
+
+}
